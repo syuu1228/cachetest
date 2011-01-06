c45e64d9f6ea31543f69021e3491f23a28f28bc5
syuu <syuu@syuu-ThinkPad-X200.(none)>
2011-01-06 23:33:04 +0900

    fincoretest added

--- client.c
+++ client.c
@@ -8,130 +8,134 @@
 #include <fcntl.h>
 #include "common.h"
 
+#define NUM_REQS (5 * 1024)
+
 int main(int argc, char **argv)
 {
-  char *mem_pool;
-  int i, fd;
-  struct sockaddr_in addr;
+	char *mem_pool;
+	int i, fd;
+	struct sockaddr_in addr;
 
-  if (argc != 3) {
-    fprintf(stderr, "argument required\n");
-    return -1;
-  }
+	if (argc != 3) {
+		fprintf(stderr, "argument required\n");
+		return -1;
+	}
 	
-  if (mlockall(MCL_CURRENT | MCL_FUTURE)) {
-    perror("mlockall");
-    return -1;
-  }
+	if (mlockall(MCL_CURRENT | MCL_FUTURE)) {
+		perror("mlockall");
+		return -1;
+	}
 	
-  mem_pool = (char *)malloc(POOL_SIZE);
-  if (!mem_pool) {
-    perror("malloc");
-    return -1;
-  }
+	mem_pool = (char *)malloc(POOL_SIZE);
+	if (!mem_pool) {
+		perror("malloc");
+		return -1;
+	}
+	addr.sin_family = AF_INET;
+	addr.sin_port = htons(SERVER_PORT);
+	addr.sin_addr.s_addr = inet_addr(argv[1]);
 
-  fd = open("/dev/sda", O_RDONLY);
-  if (fd < 0) {
-    perror("open");
-    return fd;
-  }
+	if (!strcmp(argv[2], "put")) {
+		fd = open("/dev/sda", O_RDONLY);
+		if (fd < 0) {
+			perror("open");
+			return fd;
+		}
 	
-  if((read(fd, (void *)mem_pool, POOL_SIZE)) < POOL_SIZE) {
-    perror("read");
-    close(fd);
-    return -1;
-  }
+		if((read(fd, (void *)mem_pool, POOL_SIZE)) < POOL_SIZE) {
+			perror("read");
+			close(fd);
+			return -1;
+		}
 	
-  close(fd);
+		close(fd);
 
-  addr.sin_family = AF_INET;
-  addr.sin_port = htons(SERVER_PORT);
-  addr.sin_addr.s_addr = inet_addr(argv[1]);
+		for (i = 0; i < NUM_OBJ; i++) {
+			struct packet p;
+			ssize_t siz;
+			char *obj;
 
-  if (!strcmp(argv[2], "put")) {
-    for (i = 0; i < NUM_OBJ; i++) {
-      struct packet p;
-      ssize_t siz;
-      char *obj;
-		
-      p.p_type = P_TYPE_PUT;
-      p.p_seq = i;
-      obj = mem_pool + (OBJ_SIZE * i);
+			p.p_type = P_TYPE_PUT;
+			p.p_seq = i;
+			obj = mem_pool + (OBJ_SIZE * i);
 
-      if ((fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-	perror("socket");
-	return -1;
-      }
+			if ((fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+				perror("socket");
+				return -1;
+			}
 
-      if (connect(fd, (struct sockaddr *)&addr, sizeof(addr))) {
-	perror("connect");
-	close(fd);
-	return -1;
-      }
+			if (connect(fd, (struct sockaddr *)&addr, sizeof(addr))) {
+				perror("connect");
+				close(fd);
+				return -1;
+			}
 
-      if ((siz = send(fd, &p, sizeof(struct packet), MSG_MORE))
-	  != sizeof(struct packet)) {
-	perror("send");
-	printf("siz:%zd actual:%zd\n", siz, sizeof(struct packet));
-	close(fd);
-	return -1;
-      }
+			if ((siz = send(fd, &p, sizeof(struct packet), MSG_MORE))
+				!= sizeof(struct packet)) {
+				perror("send");
+				printf("siz:%zd actual:%zd\n", siz, sizeof(struct packet));
+				close(fd);
+				return -1;
+			}
 		
-      if (send_obj(fd, obj, (size_t)OBJ_SIZE, 0)) {
-	perror("send");
-	close(fd);
-	return -1;
-      }
+			if (send_obj(fd, obj, (size_t)OBJ_SIZE, 0)) {
+				perror("send");
+				close(fd);
+				return -1;
+			}
 
-      close(fd);
-    }
-  }
-	
-  for (i = 0; i < NUM_OBJ; i++) {
-    struct packet p;
-    ssize_t siz;
-    char *obj;
-    char buf[OBJ_SIZE];
-    int ret;
-		
-    p.p_type = P_TYPE_GET;
-    p.p_seq = i;
-    obj = mem_pool + (OBJ_SIZE * i);
+			close(fd);
+		}
+	}else if (!strcmp(argv[2], "get_rand")) {
+		int schedule[NUM_REQS];
 
-    if ((fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-      perror("socket");
-      return -1;
-    }
+		for (i = 0; i < NUM_REQS; i++) {
+			srand(i);
+			schedule[i] = rand() % NUM_OBJ;
+			printf("[%d] obj_%d\n", i, schedule[i]);
+		}
 		
+		for (i = 0; i < NUM_REQS; i++) {
+			struct packet p;
+			ssize_t siz;
+			char *obj;
+			char buf[OBJ_SIZE];
+			int ret;
 		
-    if (connect(fd, (struct sockaddr *)&addr, sizeof(addr))) {
-      perror("connect");
-      close(fd);
-      return -1;
-    }
-		
-    if ((siz = send(fd, &p, sizeof(struct packet), 0))
-	!= sizeof(struct packet)) {
-      perror("send");
-      printf("siz:%zd actual:%zd\n", siz, sizeof(struct packet));
-      close(fd);
-      return -1;
-    }
-		
-    if (recv_obj(fd, buf, OBJ_SIZE, 0)) {
-      perror("recv");
-      close(fd);
-      return -1;
-    }
+			p.p_type = P_TYPE_GET;
+			p.p_seq = schedule[i];
+			obj = mem_pool + (OBJ_SIZE * schedule[i]);
 
-    if ((ret = memcmp(obj, buf, OBJ_SIZE))) {
-      printf("object differ: %d\n", ret);
-      close(fd);
-      return -1;
-    }else{
-      printf("%d: OK\n", i);
-    }
+			if ((fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+				perror("socket");
+				return -1;
+			}
 		
-    close(fd);
-  }
+		
+			if (connect(fd, (struct sockaddr *)&addr, sizeof(addr))) {
+				perror("connect");
+				close(fd);
+				return -1;
+			}
+		
+			if ((siz = send(fd, &p, sizeof(struct packet), 0))
+				!= sizeof(struct packet)) {
+				perror("send");
+				printf("siz:%zd actual:%zd\n", siz, sizeof(struct packet));
+				close(fd);
+				return -1;
+			}
+		
+			if (recv_obj(fd, buf, OBJ_SIZE, 0)) {
+				perror("recv");
+				close(fd);
+				return -1;
+			}
+		
+			close(fd);
+		}
+	}else if (!strcmp(argv[2], "get_zipfs")) {
+		double percentages[NUM_OBJ];
+		
+	}
 }
